7e75a15c42ac06214fe4fe24f23d8b31f8de0eaa
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-12-21 19:25:04 +0700

    first commit

---
+++ src/main/java/com/codegym/cms/service/IGeneralService.java
@@ -0,0 +1,18 @@
+package com.codegym.cms.service;
+
+import com.codegym.cms.model.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface IGeneralService<T> {
+    Iterable<T> findAll();
+
+    Optional<T> findById(Long id);
+
+    void save(T t);
+
+    void remove(Long id);
+
+}
